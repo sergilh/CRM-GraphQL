8d35e1e1f6ad55c4896000afd42d0dcbca87fb5b
Sergi López Hernández <serkio.lh@gmail.com>
2025-04-29 19:50:05 +0200

    Commit inicial

--- index.js
+++ index.js
@@ -41,7 +41,7 @@ const server = new ApolloServer({
         origin: [
             'https://crm-clientes-rosy.vercel.app',
             'https://crm-clientes-4dsu-3voawmz1c-sergi-lopez-hernandezs-projects.vercel.app',
-            '*',
+            ,
         ],
         credentials: true,
     },
